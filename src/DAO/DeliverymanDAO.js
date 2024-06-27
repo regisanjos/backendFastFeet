3b917ce3042dd82b4884a25f04615dfb1de34588
regis <regisanjos47@gmai.com>
2024-06-27 01:34:06 -0300

    implementando delivDAO

---
+++ src/DAO/DeliverymanDAO.js
@@ -0,0 +1,44 @@
+const {
+    createDeliveryman,
+    getDeliverymen,
+    getDeliverymanById,
+    updateDeliveryman,
+    deleteDeliveryman,
+} = require('../BLL/deliverymanBLL');
+
+const create = async (req, res) => {
+    const deliveryman = await createDeliveryman(req.body);
+    res.status(201).json(deliveryman);
+};
+
+const getAll = async (req, res) => {
+    const deliverymen = await getDeliverymen();
+    res.json(deliverymen);
+};
+
+const getById = async (req, res) => {
+    const deliveryman = await getDeliverymanById(req.params.id);
+    if (deliveryman) {
+        res.json(deliveryman);
+    } else {
+        res.status(404).json({ message: 'Deliveryman not found' });
+    }
+};
+
+const update = async (req, res) => {
+    const deliveryman = await updateDeliveryman(req.params.id, req.body);
+    res.json(deliveryman);
+};
+
+const remove = async (req, res) => {
+    await deleteDeliveryman(req.params.id);
+    res.status(204).send();
+};
+
+module.exports = {
+    create,
+    getAll,
+    getById,
+    update,
+    remove,
+};
