26a54a7d537ebe473a1e734c95d6e83d34e4c141
regis <regisanjos47@gmai.com>
2024-06-27 02:02:01 -0300

    implementando

---
+++ src/router/routerDeliveryman.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const { create, getAll, getById, update, remove } = require('../controllers/DeliverymanController');
+const { isAdmin, isAuthenticated } = require('../middlewares/authMiddleware');
+
+router.use(isAuthenticated);
+router.use(isAdmin);
+
+router.post('/', create);
+router.get('/', getAll);
+router.get('/:id', getById);
+router.put('/:id', update);
+router.delete('/:id', remove);
+
+module.exports = router;
