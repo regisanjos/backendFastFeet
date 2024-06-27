59ce2575c7edb5b8be7e460ede7233f79157d464
regis <regisanjos47@gmai.com>
2024-06-27 01:18:15 -0300

    refatorando recipientContr

--- src/controllers/RecipientController.js
+++ src/controllers/RecipientController.js
@@ -1,56 +1,38 @@
-const express = require('express');
-const router = express.Router();
-const recipientService = require('../BLL/recipientService');
-const { authenticateToken, authorizeRole } = require('../middleware/auth');
+const { prisma } = require('../prismaClient');
 
-// Create Recipient
-router.post('/', authenticateToken, authorizeRole('ADMIN'), async (req, res) => {
-  try {
-    const recipient = await recipientService.createRecipient(req.body);
-    res.status(201).json(recipient);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+const createRecipient = async (data) => {
+  return await prisma.recipient.create({
+    data,
+  });
+};
 
-// Get All Recipients
-router.get('/', authenticateToken, authorizeRole('ADMIN'), async (req, res) => {
-  try {
-    const recipients = await recipientService.getRecipients();
-    res.status(200).json(recipients);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+const getAllRecipients = async () => {
+  return await prisma.recipient.findMany();
+};
 
-// Get Recipient by ID
-router.get('/:id', authenticateToken, authorizeRole('ADMIN'), async (req, res) => {
-  try {
-    const recipient = await recipientService.getRecipientById(req.params.id);
-    res.status(200).json(recipient);
-  } catch (error) {
-    res.status(404).json({ error: error.message });
-  }
-});
+const getRecipientById = async (id) => {
+  return await prisma.recipient.findUnique({
+    where: { id },
+  });
+};
 
-// Update Recipient
-router.put('/:id', authenticateToken, authorizeRole('ADMIN'), async (req, res) => {
-  try {
-    const recipient = await recipientService.updateRecipient(req.params.id, req.body);
-    res.status(200).json(recipient);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+const updateRecipient = async (id, data) => {
+  return await prisma.recipient.update({
+    where: { id },
+    data,
+  });
+};
 
-// Delete Recipient
-router.delete('/:id', authenticateToken, authorizeRole('ADMIN'), async (req, res) => {
-  try {
-    await recipientService.deleteRecipient(req.params.id);
-    res.status(204).send();
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+const deleteRecipient = async (id) => {
+  return await prisma.recipient.delete({
+    where: { id },
+  });
+};
 
-module.exports = router;
+module.exports = {
+  createRecipient,
+  getAllRecipients,
+  getRecipientById,
+  updateRecipient,
+  deleteRecipient,
+};
