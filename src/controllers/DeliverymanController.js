708fefb3f11d2c0a83ce0cbe2521aa07b3a48afc
regis <regisanjos47@gmai.com>
2024-06-27 01:50:27 -0300

    Refatorando

--- src/controllers/DeliverymanController.js
+++ src/controllers/DeliverymanController.js
@@ -1,38 +1,44 @@
-const { prisma } = require('../prismaClient');
-
-const createDeliveryman = async (data) => {
-  return await prisma.deliveryman.create({
-    data,
-  });
-};
-
-const getAllDeliverymen = async () => {
-  return await prisma.deliveryman.findMany();
-};
-
-const getDeliverymanById = async (id) => {
-  return await prisma.deliveryman.findUnique({
-    where: { id },
-  });
-};
-
-const updateDeliveryman = async (id, data) => {
-  return await prisma.deliveryman.update({
-    where: { id },
-    data,
-  });
-};
-
-const deleteDeliveryman = async (id) => {
-  return await prisma.deliveryman.delete({
-    where: { id },
-  });
-};
-
-module.exports = {
+const {
   createDeliveryman,
-  getAllDeliverymen,
+  getDeliverymen,
   getDeliverymanById,
   updateDeliveryman,
   deleteDeliveryman,
+} = require('../BLL/deliverymanBLL');
+
+const create = async (req, res) => {
+  const deliveryman = await createDeliveryman(req.body);
+  res.status(201).json(deliveryman);
+};
+
+const getAll = async (req, res) => {
+  const deliverymen = await getDeliverymen();
+  res.json(deliverymen);
+};
+
+const getById = async (req, res) => {
+  const deliveryman = await getDeliverymanById(req.params.id);
+  if (deliveryman) {
+    res.json(deliveryman);
+  } else {
+    res.status(404).json({ message: 'Deliveryman not found' });
+  }
+};
+
+const update = async (req, res) => {
+  const deliveryman = await updateDeliveryman(req.params.id, req.body);
+  res.json(deliveryman);
+};
+
+const remove = async (req, res) => {
+  await deleteDeliveryman(req.params.id);
+  res.status(204).send();
+};
+
+module.exports = {
+  create,
+  getAll,
+  getById,
+  update,
+  remove,
 };
