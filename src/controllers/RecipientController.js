3622df9faa3c78d2a630b81071183a2761dc418f
regis <regisanjos47@gmai.com>
2024-06-27 01:53:35 -0300

    refatorando

--- src/controllers/RecipientController.js
+++ src/controllers/RecipientController.js
@@ -1,38 +1,44 @@
-const { prisma } = require('../prismaClient');
-
-const createRecipient = async (data) => {
-  return await prisma.recipient.create({
-    data,
-  });
-};
-
-const getAllRecipients = async () => {
-  return await prisma.recipient.findMany();
-};
-
-const getRecipientById = async (id) => {
-  return await prisma.recipient.findUnique({
-    where: { id },
-  });
-};
-
-const updateRecipient = async (id, data) => {
-  return await prisma.recipient.update({
-    where: { id },
-    data,
-  });
-};
-
-const deleteRecipient = async (id) => {
-  return await prisma.recipient.delete({
-    where: { id },
-  });
-};
-
-module.exports = {
+const {
   createRecipient,
-  getAllRecipients,
+  getRecipients,
   getRecipientById,
   updateRecipient,
   deleteRecipient,
+} = require('../BLL/recipientBLL');
+
+const create = async (req, res) => {
+  const recipient = await createRecipient(req.body);
+  res.status(201).json(recipient);
+};
+
+const getAll = async (req, res) => {
+  const recipients = await getRecipients();
+  res.json(recipients);
+};
+
+const getById = async (req, res) => {
+  const recipient = await getRecipientById(req.params.id);
+  if (recipient) {
+    res.json(recipient);
+  } else {
+    res.status(404).json({ message: 'Recipient not found' });
+  }
+};
+
+const update = async (req, res) => {
+  const recipient = await updateRecipient(req.params.id, req.body);
+  res.json(recipient);
+};
+
+const remove = async (req, res) => {
+  await deleteRecipient(req.params.id);
+  res.status(204).send();
+};
+
+module.exports = {
+  create,
+  getAll,
+  getById,
+  update,
+  remove,
 };
