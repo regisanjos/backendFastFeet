835d7afdb6179caa269ae78b3fc57a7bb78bef3b
regis <regisanjos47@gmai.com>
2024-06-27 01:55:01 -0300

    refatorando

---
+++ src/controllers/UserController.js
@@ -0,0 +1,44 @@
+const {
+  createUser,
+  getUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+} = require('../BLL/userBLL');
+
+const create = async (req, res) => {
+  const user = await createUser(req.body);
+  res.status(201).json(user);
+};
+
+const getAll = async (req, res) => {
+  const users = await getUsers();
+  res.json(users);
+};
+
+const getById = async (req, res) => {
+  const user = await getUserById(req.params.id);
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).json({ message: 'User not found' });
+  }
+};
+
+const update = async (req, res) => {
+  const user = await updateUser(req.params.id, req.body);
+  res.json(user);
+};
+
+const remove = async (req, res) => {
+  await deleteUser(req.params.id);
+  res.status(204).send();
+};
+
+module.exports = {
+  create,
+  getAll,
+  getById,
+  update,
+  remove,
+};
